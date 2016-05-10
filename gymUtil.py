228d45febfde0659752d959afe70f0fe6fbbe0e1
Jia-Shen Boon <jiashen@gmail.com>
2016-05-10 16:19:24 -0500

    Method to insert a reward accumulator into env

--- gymUtil.py
+++ gymUtil.py
@@ -1,3 +1,5 @@
+import gym
+
 def transformEnvState(env, transform):
     """Transform an environment's state.
 
@@ -13,3 +15,20 @@ def transformEnvState(env, transform):
     env.step = newStep
     env.reset = newReset
 
+def insertRewardAccumulator(env):
+    """Adds a member to class which accumulates reward, and resets to 0 when
+    reset() is called
+    """
+    oldStep = env.step
+    oldReset = env.reset
+    env.total_reward_since_reset = 0
+    def newStep(*args, **kwargs):
+        ret = oldStep(*args, **kwargs)
+        reward = ret[1]
+        env.total_reward_since_reset = reward + env.total_reward_since_reset
+        return ret
+    def newReset(*args, **kwargs):
+        env.total_reward_since_reset = 0
+        return oldReset(*args, **kwargs)
+    env.step = newStep
+    env.reset = newReset
